965b69f984f5226cd66510fddb335c00ddd71912
Vivek Pal <31vivekpal@gmail.com>
2022-04-06 17:29:58 +0530

    Update Header.js

--- pages/Header.js
+++ pages/Header.js
@@ -11,7 +11,7 @@ export default function Header() {
                 <img src="shastralogo.png" />
             </a>
             <div className="alignItems">
-                <button className={styles.blueBtn} onClick={() => { window.open('https://bit.ly/shastraos', "_blank") }}><MdOutlineDownload className={styles.icon}/>Download</button>
+                <button className={styles.blueBtn} onClick={() => { window.open('https://osdn.net/frs/redir.php?m=nchc&f=shastraos%2F77103%2FShastra-pre-2022.04.02-x86_64.iso', "_blank") }}><MdOutlineDownload className={styles.icon}/>Download</button>
             </div>
         </header>
     )

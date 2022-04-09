7a5f820a6ea6ee3dd7633baed3e833b5fae58cda
vivek <31vivekpal@gmail.com>
2022-04-10 02:40:21 +0530

    🏄

--- pages/index.js
+++ pages/index.js
@@ -31,7 +31,7 @@ export default function Home() {
               {/* <span>v{versionData}</span> */}
             </div>
             <div className={styles.buttonGap}>
-              <button className={styles.blueBtn} onClick={() => { window.open('https://osdn.net/frs/redir.php?m=nchc&f=shastraos%2F77132%2FShastraDignity-Beta-2022.04.07-x86_64.iso', "_blank") }}><MdOutlineDownload className={styles.icon}/>Install</button>
+              <button className={styles.blueBtn} onClick={() => { window.open('https://osdn.net/frs/redir.php?m=nchc&f=shastraos%2F77132%2FShastraDignity-Beta-2022.04.09-x86_64.iso', "_blank") }}><MdOutlineDownload className={styles.icon}/>Install</button>
               <button className={styles.whiteBtn} onClick={() => { window.open('https://github.com/shastra-os', "_blank") }}><MdCode className={styles.icon}/>GitHub</button>
             </div>
           </section>

06034ca5f2a2309bc865dffe6684acedb2063589
Vivek Pal <vivekpal@duck.com>
2022-06-29 11:05:48 +0000

    support

--- pages/index.js
+++ pages/index.js
@@ -31,8 +31,8 @@ export default function Home() {
               {/* <span>v{versionData}</span> */}
             </div>
             <div className={styles.buttonGap}>
-              <button className={styles.blueBtn} onClick={() => { window.open('https://osdn.net/frs/redir.php?m=rwthaachen&f=shastraos%2F77132%2FShastraDignity-Beta-2022.05.07-x86_64.iso', "_blank") }}><MdOutlineDownload className={styles.icon}/>Install</button>
-              <button className={styles.whiteBtn} onClick={() => { window.open('https://github.com/shastra-os', "_blank") }}><MdCode className={styles.icon}/>GitHub</button>
+              <button className={styles.blueBtn} onClick={() => { window.open('https://osdn.net/frs/redir.php?m=rwthaachen&f=shastraos%2F77132%2FShastraDignity-Beta-2022.05.07-x86_64.iso', "_blank") }}><MdOutlineDownload className={styles.icon}/>Beta</button>
+              <button className={styles.whiteBtn} onClick={() => { window.open('https://github.com/shastra-os', "_blank") }}><MdCode className={styles.icon}/>CODE</button>
             </div>
           </section>
           <section className="mockupContainer">

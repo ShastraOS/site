1a0162963ed8a135cad0da5f03e5f21c72e37f87
Vivek Pal <vivekpal@duck.com>
2022-07-23 15:33:18 +0000

    mirror

--- pages/about.js
+++ pages/about.js
@@ -60,7 +60,7 @@ export default function About() {
                         <p>The beta release is available. It is an unstable version for now. But you can test it out and contribute to this open-source project.</p>
                         <div className={styles.buttonGap}>
                             <button className={styles.whiteBtn} onClick={() => { window.open('https://github.com/shastra-os', "_blank") }}><MdCode className={styles.icon}/>GitHub</button>
-                            <button className={styles.whiteBtn} onClick={() => { window.open('https://osdn.net/frs/redir.php?m=gigenet&f=shastraos%2F77132%2FShastra-Beta-2022.07.17-x86_64.iso', "_blank") }}><MdDownload className={styles.icon}/>Get Shastra</button>
+                            <button className={styles.whiteBtn} onClick={() => { window.open('https://osdn.net/frs/redir.php?m=gigenet&f=shastraos%2F77132%2FShastra-Beta-2022.07.22-x86_64.iso', "_blank") }}><MdDownload className={styles.icon}/>Get Shastra</button>
                         </div>
 
                         <hr />

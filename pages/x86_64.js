3add8508dfef83ddfd0b38a99e5fc54de4452b78
Vivek Pal <vivekpal@duck.com>
2022-08-08 22:00:04 +0000

    repo

--- pages/x86_64.js
+++ pages/x86_64.js
@@ -24,8 +24,7 @@ export default function About() {
                             <li><a href="https://ipfs.io/ipfs/QmaYhPuU25Frhy2dVKa8n84qpuMrqmZhYKu8kpcQNWGfvH?filename=about-shastraos-0.1-2-any.pkg.tar.zst">about-shastraos-0.1-2-any.pkg.tar.zst</a></li>
                             <li><a href="https://ipfs.io/ipfs/Qma6RCeCAAhCaDjTrKt9tKkjzxE1yjtcXoC9wqagm59W55?filename=shastrarepo.db">shastrarepo.db</a></li>
                             <li><a href="https://ipfs.io/ipfs/QmUTG3EGRLUry214sq8RX5XKUV2mirDExzE7yPY2rUMFg5?filename=shastrarepo.files">shastrarepo.files</a></li>
-                            <li><a href="">GitHub</a></li>
-                            <li><a href="">GitHub</a></li>
+                            <li><a href="">Repo</a></li>
                         </ul>
                     </div>
                 </div>

dcdf6bd118f21ae4f939d41a35b91f9ef7f0dd70
Vivek Pal <vivekpal@duck.com>
2022-09-27 06:34:23 +0000

    change form

--- pages/opensource.js
+++ pages/opensource.js
@@ -40,7 +40,7 @@ export default function Guide() {
               <button
                 className={styles.whiteBtn}
                 onClick={() => {
-                  window.open("https://uimhaxwrrxg.typeform.com/to/B1iUIP71", "_blank");
+                  window.open("https://shastraos.budibase.app/app/hacktoberfest2022", "_blank");
                 }}
               >
                 <MdCode className={styles.icon} />

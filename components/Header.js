298801b9608ae010c67124f9171c6d794d9e4c9a
Vivek Pal <vivekpal@duck.com>
2022-10-17 21:54:25 +0000

    icon header

--- components/Header.js
+++ components/Header.js
@@ -22,7 +22,7 @@ export default function Header() {
     return (
         <header>
             <Link href="/">
-                <img src="icons/shastralogo.png" />
+                <img src="icons/icon-indigo.svg" />
             </Link>
             <div className="alignItems">
                 <button className={styles.blueBtn} onClick={() => { window.open('https://www.reddit.com/r/shastraos', "_blank") }}><FaReddit className={styles.icon}/>REDDIT</button>        

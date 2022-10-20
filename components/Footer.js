86ad0c67b20c9850eb3fe9ec675c15432a55e5a9
Aditya <aditya.pandey.241103@gmail.com>
2022-10-20 23:43:42 +0530

    Responsive Footer

--- components/Footer.js
+++ components/Footer.js
@@ -2,20 +2,19 @@ import React from "react";
 import Link from "next/link"
 import styles from "../styles/Home.module.css";
 
-const stickyfooter
-={
-  position:"fixed",
-    right: "0",
-    marginTop: "91vh",
-    left: "0",
-    padding: "1rem",
-    textAlign: "center",
+// const stickyfooter
+// ={
+//   position:"fixed",
+//     right: "0",
+//     marginTop: "91vh",
+//     left: "0",
+//     padding: "1rem",
+//     textAlign: "center",
 
-}
+// }
 export default function Footer() {
   return (
-    <footer style={stickyfooter
-}>
+    <footer>
       <div className="alignItems-f" >
         <Link href="/">Home</Link>
         <Link href="install">Guide</Link>

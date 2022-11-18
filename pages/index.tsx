4b3233e938242adff75fc7f7c92bbccef9475f21
Vivek Pal <vivekpal@duck.com>
2022-11-18 00:51:50 +0000

    rm unnecessary things

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,6 @@ import { Box, Card, Flex, Heading, Container, Section, Separator, Grid, Link, Pa
 import { Titles } from '@components/Titles';
 import { Header } from '@components/Header';
 import { Hero } from '@components/Hero';
-// import { FeaturesGrid } from '@components/Features';
-// import { Community } from '@components/Community';
 import { StarFilledIcon } from '@modulz/radix-icons';
 
 const variantsCode = `const Button = styled('button', {
